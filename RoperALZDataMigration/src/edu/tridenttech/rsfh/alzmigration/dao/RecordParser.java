0ac47a375fc5a88851fc35892f6f9294db2513e7
KingPin <KingPin@EmberCelica>
2018-07-31 12:27:23 -0400

    added verification for first name, last name, race, gender, and parses the Address

--- RoperALZDataMigration/src/edu/tridenttech/rsfh/alzmigration/dao/RecordParser.java
+++ RoperALZDataMigration/src/edu/tridenttech/rsfh/alzmigration/dao/RecordParser.java
@@ -1,5 +1,10 @@
 package edu.tridenttech.rsfh.alzmigration.dao;
 
+import java.text.ParseException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import edu.tridenttech.rsfh.alzmigration.logging.AnomalyLogger;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 
@@ -7,9 +12,18 @@ public class RecordParser {
 	
 	ExistingParticipantRecord exitsting = new ExistingParticipantRecord();
 	
+	
+	
+	
+	Pattern addressPattern = Pattern.compile ("^(?add[0-9]{1,6} [a-zA-Z]??[.]??[ ]?[a-zA-Z]+? [a-zA-Z]{2,7}[.]?[,]?)(?city[a-zA-Z]+[ ]?[a-zA-Z]*[,]?)(?state[a-zA-Z]{2}[.]??)(?zip[0-9]{5})$");
+
+
+	
+	
+	
 //	newParticipantRecord newRecord = new newParticipantRecord();
 	
-	String rowText="";
+//	String rowText="";
 	
 //	String firstName = "";
 //	String lastName = "";
@@ -29,12 +43,16 @@ public class RecordParser {
 
 
 
-private NewParticipantRecord parse(ExistingParticipantRecord oldRec)
+public NewParticipantRecord parse(ExistingParticipantRecord oldRec)
 {
 	NewParticipantRecord newRecord = new NewParticipantRecord();
 //	tempText = ("^(?newRecord.setFirstName[a-zA-Z-.]) (?newRecord.setLastName[a-zA-Z-.])$");
 	
 	
+	verifyFirstName(newRecord, oldRec.getFirstName());
+	verifyLastName(newRecord, oldRec.getLastName());
+	
+	parseAddress(newRecord, oldRec.getAddress());
 	
 	
 	
@@ -44,40 +62,87 @@ private NewParticipantRecord parse(ExistingParticipantRecord oldRec)
 	return newRecord;
 }
 
-private String verifyFirstName(String tempName)
+private String verifyFirstName(NewParticipantRecord newRec, String tempName)
 {
 	if(tempName.equals(""))
 	{
-		String msg = String.format("Problem with First Name");
-		Alert emailError = new Alert(AlertType.ERROR, msg);
-		emailError.showAndWait();
+		AnomalyLogger.getInstance().Log(exitsting, AnomalyLogger.ErrorType.ADDRESS, "Problem with First Name or empty");
+		return null;
 	}
-	return tempName;
+	newRec.setFirstName(tempName);
+	return null;
 }
 
 
-private String verifyLastName(String tempName)
+private String verifyLastName(NewParticipantRecord newRec, String tempName)
 {
 	if(tempName.equals(""))
 	{
-		String msg = String.format("Problem with Last Name");
-		Alert emailError = new Alert(AlertType.ERROR, msg);
-		emailError.showAndWait();
+		AnomalyLogger.getInstance().Log(exitsting, AnomalyLogger.ErrorType.ADDRESS, "Problem with Last Name or empty");
+		return null;
 	}
-	
-	return tempName;
+	newRec.setLastName(tempName);
+	return null;
 }
+
+private String verifyGender(NewParticipantRecord newRec, String tempGen)
+{
+	if(tempGen.equals(""))
+	{
+		AnomalyLogger.getInstance().Log(exitsting, AnomalyLogger.ErrorType.ADDRESS, "Problem with Race or empty");
+		return null;
+	}
+	newRec.setGender(tempGen);
+	return null;
+}
+
+private String verifyRace(NewParticipantRecord newRec, String tempRace)
+{
+	if(tempRace.equals(""))
+	{
+		AnomalyLogger.getInstance().Log(exitsting, AnomalyLogger.ErrorType.ADDRESS, "Problem with Gender or empty");
+		return null;
+	}
+	newRec.setRace(tempRace);
+	return null;
 }
 
 
-//private String parseAddress(String tempAddress)
-//{
-//	
-//	
-//	
-//	
-//	return tempAddress
-//}
+
+
+
+
+
+private String parseAddress(NewParticipantRecord newRec, String tempAddress)
+{
+	//	String add = "";
+	//	String city = "";
+	//	String state = "";
+	//	String zip = "";
+
+	Matcher addressMatcher = addressPattern.matcher(tempAddress);
+
+	if(addressMatcher.matches())
+	{
+		//		newRec.setAddress() = addressMatcher.group("add");
+		newRec.setAddress(addressMatcher.group("add"));
+		//		city = addressMatcher.group("city");
+		newRec.setCity(addressMatcher.group("city"));
+		//		state = addressMatcher.group("state");
+		newRec.setState(addressMatcher.group("state"));
+		//		zip = addressMatcher.group("zip");
+		newRec.setZip(addressMatcher.group("zip"));
+
+	}
+	else
+	{
+		AnomalyLogger.getInstance().Log(exitsting, AnomalyLogger.ErrorType.ADDRESS, "Problem with Address");
+		return null;
+	}
+
+
+return null;
+}
 
 
 
@@ -99,3 +164,4 @@ private String verifyLastName(String tempName)
 
 // make participant instance variable
 
+}
