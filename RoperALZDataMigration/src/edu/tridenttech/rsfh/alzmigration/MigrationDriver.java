28216aa683d463f484877ec80a30d948d27949d1
a jackson <anderson.jackson@tridenttech.edu>
2018-08-02 10:53:54 -0400

    Moved code up into the try.

--- RoperALZDataMigration/src/edu/tridenttech/rsfh/alzmigration/MigrationDriver.java
+++ RoperALZDataMigration/src/edu/tridenttech/rsfh/alzmigration/MigrationDriver.java
@@ -11,7 +11,6 @@ import edu.tridenttech.rsfh.alzmigration.dao.ExistingParticipantRecord;
 import edu.tridenttech.rsfh.alzmigration.dao.NewParticipantRecord;
 import edu.tridenttech.rsfh.alzmigration.dao.RecordParser;
 import edu.tridenttech.rsfh.alzmigration.logging.AnomalyLogger;
-import edu.tridenttech.rsfh.alzmigration.logging.AnomalyLogger.ErrorType;
 
 
 public class MigrationDriver 
@@ -20,44 +19,39 @@ public class MigrationDriver
 	public MigrationDriver(String loggerPath) throws FileNotFoundException
 	{
 		AnomalyLogger.getInstance(loggerPath);
-		
+
 	}
 
-	
-	public void performMigration(String oldFile, String newFile)
-    {
-	RecordParser parser = new RecordParser();
-	ExcelReader reader;
-	ExcelWriter writer;
-	try {
-		reader = new ExcelReader(oldFile);
-	} catch (InvalidFormatException e) {
-		e.printStackTrace();
-		System.err.println("File not found");
-	} catch (IOException e) {
-		e.printStackTrace();
-		System.err.println("File not found");
-	}
-	try {
-		writer = new ExcelWriter(newFile);
-	} catch (InvalidFormatException e) {
-		e.printStackTrace();
-		System.err.println("File not found");
-	} catch (FileNotFoundException e) {
-		e.printStackTrace();
-		System.err.println("File not found");
-	} catch (IOException e) {
-		e.printStackTrace();
-		System.err.println("File not found");
-	}
-	while (reader.hasMoreRecords()) {
-	ExistingParticipantRecord record = reader.getNextRecord();
 
-	NewParticipantRecord participant = parser.parse(record);
-	writer.writeRecord(participant);
-	}
+	public void performMigration(String oldFile, String newFile) throws IOException
+	{
+		RecordParser parser = new RecordParser();
+		ExcelReader reader = null;
+		ExcelWriter writer = null;
+		try {
+			reader = new ExcelReader(oldFile);
+			writer = new ExcelWriter(newFile);
+			while (reader.hasMoreRecords()) {
+				ExistingParticipantRecord record = reader.getNextRecord();
 
-	reader.close();
+				NewParticipantRecord participant = parser.parse(record);
+				writer.writeRecord(participant);
+			}
+		} catch (InvalidFormatException e) {
+			e.printStackTrace();
+			System.err.println("File not found");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.err.println("File not found");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("File not found");
+		} finally {
+			if (reader != null)
+				reader.close();
+		}
+
+
 
 	}
 }
