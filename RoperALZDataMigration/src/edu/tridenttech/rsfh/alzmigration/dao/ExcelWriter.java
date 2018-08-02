4bce9e0f0bfc7023cf4a221056d4524b5a64c362
Kevin Franks <k_franks@outlook.com>
2018-08-02 00:59:25 -0400

    Mapped writer for status and test score sheets

--- RoperALZDataMigration/src/edu/tridenttech/rsfh/alzmigration/dao/ExcelWriter.java
+++ RoperALZDataMigration/src/edu/tridenttech/rsfh/alzmigration/dao/ExcelWriter.java
@@ -39,6 +39,8 @@ public class ExcelWriter
 		try
 		{
 					
+			//primary sheet
+			
 			Sheet sheet = wb.getSheetAt(0);
 			
 			int rowCount = sheet.getLastRowNum();
@@ -71,28 +73,90 @@ public class ExcelWriter
 			cell.setCellValue(rd.getAddress());
 			
 			cell = row.createCell(8);
-			cell.setCellValue(rd.getEmail());
+			cell.setCellValue(rd.getCity());
 			
 			cell = row.createCell(9);
-			cell.setCellValue(rd.getPhone());
-			
-			cell = row.createCell(15);
-			cell.setCellValue(rd.getScores());
-			
-			cell = row.createCell(16);
-			cell.setCellValue(rd.getStatus());
+			cell.setCellValue(rd.getState());
 			
 			cell = row.createCell(10);
-			cell.setCellValue(rd.getPcp());
+			cell.setCellValue(rd.getZip());
 			
 			cell = row.createCell(11);
-			cell.setCellValue(rd.getSpec());
+			cell.setCellValue(rd.getEmail());
 			
 			cell = row.createCell(12);
-			cell.setCellValue(rd.getReferal());
+			cell.setCellValue(rd.getPhone());
 			
 			cell = row.createCell(13);
+			cell.setCellValue(rd.getStatus());
+			
+			//cell = row.createCell(14);
+			//cell.setCellValue(rd.Deseased());
+			
+			cell = row.createCell(15);
+			cell.setCellValue(rd.getPcp());
+			
+			cell = row.createCell(16);
+			cell.setCellValue(rd.getSpec());
+			
+			cell = row.createCell(17);
+			cell.setCellValue(rd.getReferal());
+			
+			cell = row.createCell(18);
 			cell.setCellValue(rd.getMailing());
+			
+			
+			//Status Sheet
+            sheet = wb.getSheetAt(4);
+			
+			rowCount = sheet.getLastRowNum();
+			
+			row = sheet.createRow(++rowCount);
+			
+			
+			cell = row.createCell(2);
+			cell.setCellValue(rd.getLastName());
+			
+			cell = row.createCell(1);
+			cell.setCellValue(rd.getFirstName());
+			
+			cell = row.createCell(3);
+			cell.setCellValue(rd.getDob());
+			cell.setCellStyle(cellStyle);
+			
+			
+			//Test Scores
+            sheet = wb.getSheetAt(5);
+			
+			rowCount = sheet.getLastRowNum();
+			
+			row = sheet.createRow(++rowCount);
+			
+			
+			cell = row.createCell(2);
+			cell.setCellValue(rd.getLastName());
+			
+			cell = row.createCell(1);
+			cell.setCellValue(rd.getFirstName());
+			
+			cell = row.createCell(3);
+			cell.setCellValue(rd.getDob());
+			cell.setCellStyle(cellStyle);
+			
+			cell = row.createCell(4);
+			cell.setCellValue(rd.getMmseDate());
+			cell.setCellStyle(cellStyle);
+			
+			cell = row.createCell(5);
+			cell.setCellValue(rd.getMmseScore());
+			
+			cell = row.createCell(6);
+			cell.setCellValue(rd.getScaDate());
+			cell.setCellStyle(cellStyle);
+			
+			cell = row.createCell(7);
+			cell.setCellValue(rd.getScaScore());
+			
 			
 			inputStream.close();
 			
