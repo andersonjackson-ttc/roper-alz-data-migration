b8eaaba1f870733080f84f6d61465a9d36f629a7
KingPin <KingPin@EmberCelica>
2018-08-01 22:10:29 -0400

    modified the address RegEx to accept more formats

--- RoperALZDataMigration/src/edu/tridenttech/rsfh/alzmigration/dao/RecordParser.java
+++ RoperALZDataMigration/src/edu/tridenttech/rsfh/alzmigration/dao/RecordParser.java
@@ -16,7 +16,8 @@ public class RecordParser {
 	
 	
 	
-	Pattern addressPattern = Pattern.compile ("^(?add[0-9]{1,6} [a-zA-Z]??[.]??[ ]?[a-zA-Z]+? [a-zA-Z]{2,7}[.]?[,]?)(?city[a-zA-Z]+[ ]?[a-zA-Z]*[,]?)(?state[a-zA-Z]{2}[.]??)(?zip[0-9]{5})$");
+	Pattern addressPattern = Pattern.compile ("^(?add[0-9]{1,6} [a-zA-Z]??[.]??[ ]?[a-zA-Z. ]+? [a-zA-Z]{2,7}[.]?[,]? )(?city[a-zA-Z]+[ ]?[a-zA-Z]*[,]?)(?state[a-zA-Z]{2}[.]??)(?zip([0-9]{5})?)$");                 
+//												     [0-9]{1,6} [a-zA-Z]??[.]??[ ]?[a-zA-Z. ]+? [a-zA-Z]{2,7}[.]?[,]?        [a-zA-Z]+[ ]?[a-zA-Z]*[,]?        [a-zA-Z]{2}[.]??      ([0-9]{5})?
 	Pattern emailPattern = Pattern.compile("^[a-zA-Z_0-9-.]*@[a-zA-Z_0-9.-]*.[a-zA-Z]*$");
 	Pattern phonePattern = Pattern.compile("^[0-9]{3}-[0-9]{3}-[0-9]{4}$");
 	Pattern scoresPattern = Pattern.compile("^(?date([0-9]{1,3}[/-]?[0-9]{1,3}[/-]([0-9]{2}|[0-9]{4}))*)[-]?[ ]?(?score([[0-9]{1,2}]*?/[[0-9]{1,2}]*?)*)$");
